b426fa63a2900f7f6f490e3c85f17464286d6a29
Vikas Prasad <prasadvikas115@gmail.com>
2025-10-09 13:54:48 +0530

    rm c

--- client/src/Hooks/useMobile.jsx
+++ client/src/Hooks/useMobile.jsx
@@ -1,27 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// const useMobile = (breakpoint = 768) => {
-//     const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
-
-//     const handleResize = () => { // ✅ Fixed typo (handelResize ➝ handleResize)
-//         setIsMobile(window.innerWidth < breakpoint);
-//     }
-
-//     useEffect(() => {
-//         handleResize();
-//         window.addEventListener("resize", handleResize);
-
-//         return () => {
-//             window.removeEventListener("resize", handleResize);
-//         };
-//     }, [breakpoint]);
-
-//     return [isMobile]; // ✅ Return value
-// };
-
-// export default useMobile;
-
-
 import { useEffect, useState } from "react";
 
 const useMobile = (breakpoint = 768) => {
